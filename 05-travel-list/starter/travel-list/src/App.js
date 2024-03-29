1c35c0e13f7a852e2568fbb0881961671e0a6383
Adeyemi Joshua <adeyemijoshua9999@gmail.com>
2024-03-29 21:25:45 +0100

    initialized travel list App

---
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴Far Away👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>👜You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
